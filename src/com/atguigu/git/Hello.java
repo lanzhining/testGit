dcdc9f58e286c2e520ac2b7ff9df5f072f6ba063
lanzhining <953031454@qq.com>
2016-09-18 19:28:12 +0800

    update 003

--- src/com/atguigu/git/Hello.java
+++ src/com/atguigu/git/Hello.java
@@ -5,5 +5,7 @@ public class Hello {
 		System.out.println("This is a new file !");
 		System.out.println("This is a new file !");
 		System.out.println("This is a new file !");
+		System.out.println("This is a new file !");
+		System.out.println("This is a new file !");
 	}
 }
